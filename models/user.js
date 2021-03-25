5a3675cb7a27a2797af105de6414437b254c262e
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-03-26 02:24:52 +0530

    required commit

--- models/user.js
+++ models/user.js
@@ -1,18 +1,15 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
-  firstName: {
+  username: {
     type: String,
     required: true,
   },
   lastName: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
-    unique: true,
-    required: true,
   },
   password: {
     type: String,
