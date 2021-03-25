eadabb99ea6b7143c3f671c3002f9a8a35d115c3
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-03-26 02:14:56 +0530

    use cors

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const userRoutes = require("./Routes/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const urls = [
+  /\localhost/,
+];
+
+app.use(
+  cors({
+    origin: urls,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
